5fa50cd3378244b88a84daaf0a7855eaa3637c89
DavelRad <davel.radindra2@gmail.com>
2024-09-11 00:10:43 -0700

    Fixed stuff

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-o(tk5x5!z_y08zoa!dhzt4lgk@i)l)tw95lot6d5iv+5)bpovr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
